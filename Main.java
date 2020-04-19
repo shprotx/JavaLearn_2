ba5e9798231f4a9ec82cc16d1c3dafa90095ddd7
shprotx <59147207+shprotx@users.noreply.github.com>
2020-04-19 20:32:26 +0300

    Lesson5 file

---
+++ Main.java
@@ -0,0 +1,79 @@
+package Lesson5;
+
+import java.util.Arrays;
+
+public class Main{
+
+    static final int size = 10000000;
+    static final int h = size / 2;
+
+
+    public static void main(String[] args) {
+        method1();
+        method2();
+    }
+
+    static void method1() {
+        float[] arr = new float[size];
+        Arrays.fill(arr, 1);
+
+        long a = System.currentTimeMillis();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+
+        System.out.println(System.currentTimeMillis() - a);
+    }
+
+    static void method2() {
+        float[] arr = new float[size];
+        float[] a1 = new float[h];
+        float[] a2 = new float[h];
+
+        Arrays.fill(arr, 1);
+
+        long a = System.currentTimeMillis();
+
+        System.arraycopy(arr, 0, a1, 0, h);
+        System.arraycopy(arr, h, a2, 0, h);
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Thread 1 started");
+                for (int i = 0; i < a1.length; i++) {
+                    a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Thread 2 started");
+                for (int i = 0; i < a2.length; i++) {
+                    a2[i] = (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        });
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        System.arraycopy(a1, 0, arr, 0, h);
+        System.arraycopy(a2, 0, arr, h, h);
+
+        System.out.println(System.currentTimeMillis() - a);
+
+        t1.interrupt();
+        t2.interrupt();
+    }
+
+}
